7c3e55c6ec285b650ecf876ad2bc8d4fa0f8c2e5
tyfzpb <ty_fzpb@163.com>
2021-06-15 19:42:43 +0800

    spring-native demo

---
+++ spring-native/sample/src/main/java/com/github/tyfzpb/springnative/sample/SampleApplicaiton.java
@@ -0,0 +1,12 @@
+package com.github.tyfzpb.springnative.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleApplicaiton {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleApplicaiton.class,args);
+    }
+}
---
+++ spring-native/sample/src/main/java/com/github/tyfzpb/springnative/sample/controoler/HelloController.java
@@ -0,0 +1,14 @@
+package com.github.tyfzpb.springnative.sample.controoler;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String hello() {
+        return "hello, spring native demo";
+    }
+}
